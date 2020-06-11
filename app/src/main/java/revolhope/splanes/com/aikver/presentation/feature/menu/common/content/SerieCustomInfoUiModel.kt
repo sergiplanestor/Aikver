b92f0dd6d95f017849fbea0869cb3941b2db3822
splanes <sergi.planestor@gmail.com>
2020-06-11 23:24:59 +0200

    Serie wrapper model to handle custom info

---
+++ app/src/main/java/revolhope/splanes/com/aikver/presentation/feature/menu/common/content/SerieCustomInfoUiModel.kt
@@ -0,0 +1,12 @@
+package revolhope.splanes.com.aikver.presentation.feature.menu.common.content
+
+import revolhope.splanes.com.core.domain.model.content.Network
+import revolhope.splanes.com.core.domain.model.content.serie.Serie
+
+data class SerieCustomInfoUiModel(
+    val serie: Serie?,
+    val haveSeen: Boolean = false,
+    val score: Int = -1,
+    val network: Network = Network.UNKNOWN,
+    val comments: String = ""
+)
