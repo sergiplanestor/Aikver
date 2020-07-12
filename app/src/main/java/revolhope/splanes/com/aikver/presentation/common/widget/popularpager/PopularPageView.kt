43462d0ccb4cf9e73b8ab7b00e376a617324cab2
splanes <sergi.planestor@gmail.com>
2020-07-13 01:08:01 +0200

    new animation

--- app/src/main/java/revolhope/splanes/com/aikver/presentation/common/widget/popularpager/PopularPageView.kt
+++ app/src/main/java/revolhope/splanes/com/aikver/presentation/common/widget/popularpager/PopularPageView.kt
@@ -1,21 +1,20 @@
 package revolhope.splanes.com.aikver.presentation.common.widget.popularpager
 
 import android.content.Context
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
 import android.util.AttributeSet
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.AutoTransition
-import androidx.transition.TransitionManager
-//import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentCardView
-import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentIcon
-//import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentLayout
-//import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentMinify
+import android.transition.Fade
+import kotlinx.android.synthetic.main.component_popular_pageview.view.arrow
 import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentRecycler
-import kotlinx.android.synthetic.main.component_popular_pageview.view.popularContentTitle
+import kotlinx.android.synthetic.main.component_popular_pageview.view.separator
 import revolhope.splanes.com.aikver.R
 import revolhope.splanes.com.aikver.presentation.common.invisible
 import revolhope.splanes.com.aikver.presentation.common.visible
@@ -40,8 +39,9 @@ class PopularPageView @JvmOverloads constructor(
     }
 
     private fun initViews() {
-        /*popularContentMinify.setOnClickListener { if (currentState == State.EXPANDED) collapse() }
-        popularContentIcon.setOnClickListener { if (currentState == State.COLLAPSED) expand() }*/
+        arrow.setOnClickListener {
+            if (currentState == State.EXPANDED) collapse() else expand()
+        }
     }
 
     fun addItems(items: List<Content>) {
@@ -54,140 +54,53 @@ class PopularPageView @JvmOverloads constructor(
         popularContentRecycler.postDelayed({ popularContentRecycler.scrollNext() }, 5000)
     }
 
-    /*private fun expand() {
-        *//* Change current state *//*
-        currentState = State.EXPANDED
-        *//* Set views to gone *//*
-        popularContentRecycler.visible()
-        popularContentMinify.visible()
-        popularContentTitle.visible()
-        *//* Change image *//*
-        popularContentIcon.setImageResource(R.drawable.round_star_24)
-        *//* Change constraints *//*
-        ConstraintSet().apply {
-            clone(popularContentLayout)
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.START,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.START
-            )
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP
-            )
-            clear(popularContentIcon.id, ConstraintSet.END)
-            clear(popularContentIcon.id, ConstraintSet.BOTTOM)
-            applyTo(popularContentLayout)
-        }
-        ConstraintSet().apply {
-            clone(parent as ConstraintLayout)
-            connect(
-                id,
-                ConstraintSet.START,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.START
-            )
-            connect(
-                id,
-                ConstraintSet.END,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.END
-            )
-            connect(
-                id,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP
-            )
-            applyTo(parent as ConstraintLayout)
-        }
-        *//* Change width params *//*
-        popularContentCardView.layoutParams = popularContentCardView.layoutParams.apply {
-            width = LayoutParams.MATCH_PARENT
-        }
-        *//* Animate *//*
-        TransitionManager.beginDelayedTransition(popularContentCardView, AutoTransition().apply {
-            duration = 500
-        })
+    private fun collapse() {
+        /* Change current state */
+        currentState = State.COLLAPSED
+
+        /* Arrow animation */
+        rotateArrow(180f)
+
+        /* Hide view */
+        popularContentRecycler.invisible()
+
+        /* Show separator */
+        separator.visible()
+
+        /* Transition */
+        beginTransition()
     }
 
-    private fun collapse() {
-        *//* Change current state *//*
-        currentState = State.COLLAPSED
-        *//* Set views to gone *//*
-        popularContentRecycler.invisible()
-        popularContentMinify.invisible()
-        popularContentTitle.invisible()
-        *//* Change image *//*
-        popularContentIcon.setImageResource(R.drawable.round_star_border_24)
-        *//* Change constraints *//*
-        ConstraintSet().apply {
-            clone(popularContentLayout)
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.START,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.START
-            )
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.END,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.END
-            )
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP
-            )
-            connect(
-                popularContentIcon.id,
-                ConstraintSet.BOTTOM,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.BOTTOM
-            )
-            applyTo(popularContentLayout)
-        }
-        ConstraintSet().apply {
-            clone(parent as ConstraintLayout)
-            clear(popularContentCardView.id, ConstraintSet.START)
-            connect(
-                id,
-                ConstraintSet.END,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.END
-            )
-            connect(
-                id,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP
-            )
-            connect(
-                id,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP
-            )
-            connect(
-                id,
-                ConstraintSet.BOTTOM,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.BOTTOM
-            )
-            applyTo(parent as ConstraintLayout)
-        }
-        *//* Change width params *//*
-        popularContentCardView.layoutParams = popularContentCardView.layoutParams.apply {
-            width = LayoutParams.WRAP_CONTENT
-        }
-        *//* Animate *//*
-        TransitionManager.beginDelayedTransition(popularContentCardView, AutoTransition().apply {
+    private fun expand() {
+        /* Change current state */
+        currentState = State.EXPANDED
+
+        /* Arrow animation */
+        rotateArrow(0f)
+
+        /* Show view */
+        popularContentRecycler.visible()
+
+        /* Hide separator */
+        separator.invisible()
+
+        /* Transition */
+        beginTransition()
+    }
+
+    private fun rotateArrow(angle: Float) {
+        arrow.animate().rotation(angle).apply {
             duration = 500
-        })
-    }*/
+            interpolator = AccelerateDecelerateInterpolator()
+            start()
+        }
+    }
+
+    private fun beginTransition() {
+        if (parent is ViewGroup) {
+            TransitionManager.beginDelayedTransition(parent as ViewGroup, ChangeBounds().apply {
+                duration = 500
+            })
+        }
+    }
 }
