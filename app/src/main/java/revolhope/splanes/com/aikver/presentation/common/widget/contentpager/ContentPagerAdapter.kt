cd39bef73d4ac2f31e4b4a39890e14107cb49380
splanes <sergi.planestor@gmail.com>
2020-07-26 23:43:02 +0200

    content design updated

--- app/src/main/java/revolhope/splanes/com/aikver/presentation/common/widget/contentpager/ContentPagerAdapter.kt
+++ app/src/main/java/revolhope/splanes/com/aikver/presentation/common/widget/contentpager/ContentPagerAdapter.kt
@@ -1,5 +1,6 @@
 package revolhope.splanes.com.aikver.presentation.common.widget.contentpager
 
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import revolhope.splanes.com.aikver.R
 import revolhope.splanes.com.aikver.presentation.common.loadUrl
-import revolhope.splanes.com.core.domain.model.content.Content
-import revolhope.splanes.com.core.domain.model.content.serie.Serie
 
 class ContentPagerAdapter(
     private var items: List<ContentPagerUiModel>,
@@ -34,6 +33,7 @@ class ContentPagerAdapter(
             holder.image.loadUrl(backdrop)
             holder.punctuation.text = voteAverage
             holder.title.text = title
+            holder.title.setOnClickListener { displayTitle(it as TextView, holder.contentTitleRootView) }
             holder.icon.setImageResource(
                 if (isSerie) R.drawable.ic_tv else R.drawable.ic_film
             )
@@ -41,10 +41,19 @@ class ContentPagerAdapter(
         }
     }
 
+    private fun displayTitle(view: TextView, root: ViewGroup) {
+        with(view) {
+            maxLines = if (maxLines == 1) 5 else 1
+            invalidate()
+            TransitionManager.beginDelayedTransition(root)
+        }
+    }
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val image: ImageView by lazy { itemView.findViewById<ImageView>(R.id.image) }
         val title: TextView by lazy { itemView.findViewById<TextView>(R.id.title) }
         val icon: ImageView by lazy { itemView.findViewById<ImageView>(R.id.contentIcon) }
+        val contentTitleRootView: ViewGroup by lazy { itemView.findViewById<ViewGroup>(R.id.contentTitleRootView) }
         val punctuation: TextView by lazy { itemView.findViewById<TextView>(R.id.punctuation) }
     }
 }
